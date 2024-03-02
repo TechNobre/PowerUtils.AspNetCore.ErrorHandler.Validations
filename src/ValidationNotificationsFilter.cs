832d74f296e846c7656a2b5bbe6623985348043a
Nelson Nobre <technobre@gmail.com>
2024-03-02 22:08:20 +0000

    fix: Marked all implementations as `Obsolete`
    BREAKING CHANGE: Deprecated package

--- src/ValidationNotificationsFilter.cs
+++ src/ValidationNotificationsFilter.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Filters;
@@ -6,6 +7,7 @@ using PowerUtils.Validations;
 
 namespace PowerUtils.AspNetCore.ErrorHandler
 {
+    [Obsolete("This package has been discontinued because it never evolved, and the code present in this package does not justify its continuation. It is preferable to implement this code directly in the project if necessary.")]
     public class ValidationNotificationsFilter : IAsyncResultFilter
     {
         private readonly IValidationNotifications _validations;
