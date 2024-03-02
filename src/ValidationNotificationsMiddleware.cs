8179aeaf889feedb823e2fd6d051748d781dbd47
Nelson Nobre <technobre@gmail.com>
2024-03-02 22:08:20 +0000

    fix: Marked all implementations as `Obsolete`
    BREAKING CHANGE: Deprecated package

--- src/ValidationNotificationsMiddleware.cs
+++ src/ValidationNotificationsMiddleware.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
 using PowerUtils.Validations;
 
 namespace PowerUtils.AspNetCore.ErrorHandler
 {
+    [Obsolete("This package has been discontinued because it never evolved, and the code present in this package does not justify its continuation. It is preferable to implement this code directly in the project if necessary.")]
     public static class ValidationNotificationsMiddleware
     {
         public static IServiceCollection AddValidationNotifications(this IServiceCollection services)
